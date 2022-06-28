2e6145dbec06201b078d47300b9ed6e00972fef9
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-28 01:00:52 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+for (int i = 0; i < 10; i++)
+{
+   for (int i = 0; i < 10; i++) 
+   {
+      cout << i << " ";
+   }
+   cout << endl;
+}
+return 0;
+}
